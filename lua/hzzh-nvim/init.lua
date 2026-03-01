fd60b4ff8b61530d8e80b044e38fabccbe8e4e16
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-03-01 16:22:33 +0900

    refactor: rename

--- lua/hzzh-nvim/init.lua
+++ lua/hzzh-nvim/init.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 M.config = {
-	HLGroup = 'SpellBad',
-	PostZen = '」』）】＞',
-	PreZen = '、。：「『（【＜',
+	highlight = 'SpellBad',
+	trailing_zen = '」』）】＞',
+	leading_zen = '、。：「『（【＜',
 }
 
 M.setup = function(args) M.config = vim.tbl_deep_extend('force', M.config, args or {}) end
@@ -17,12 +17,12 @@ M.execute = function()
 	end
 
 	local hOfhz = '[[:alpha:]]'
-	local zOfhz = '[^\\u0-\\u7f' .. M.config.PostZen .. ']'
-	local zOfzh = '[^\\u0-\\u7f' .. M.config.PreZen .. ']'
+	local zOfhz = '[^\\u0-\\u7f' .. M.config.trailing_zen .. ']'
+	local zOfzh = '[^\\u0-\\u7f' .. M.config.leading_zen .. ']'
 	local hOfzh = '[[:alpha:]]'
 	local pattern = '\\v(' .. hOfhz .. '@<=' .. zOfhz .. ')|(' .. zOfzh .. '@<=' .. hOfzh .. ')'
 
-	vim.api.nvim_win_set_var(win, 'prevmatchid', vim.fn.matchadd(M.config.HLGroup, pattern, 0, -1))
+	vim.api.nvim_win_set_var(win, 'prevmatchid', vim.fn.matchadd(M.config.highlight, pattern, 0, -1))
 end
 
 -- \v([[:alnum:]]@<=[^\u0-\u7f])|([^\u0-\u7f、。：]@<=[[:alnum:]])
