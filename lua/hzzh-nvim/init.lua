ca8e8e2ee3d8b61edc4c3bbb56e5c7de2d2c6373
Shuhei Kubota <kubota.shuhei+github@gmail.com>
2026-03-14 22:02:16 +0900

    style: space x2

--- lua/hzzh-nvim/init.lua
+++ lua/hzzh-nvim/init.lua
@@ -1,9 +1,9 @@
 local M = {}
 
 M.config = {
-	highlight = 'SpellBad',
-	trailing_zen = '」』）】＞',
-	leading_zen = '、。：「『（【＜',
+  highlight = 'SpellBad',
+  trailing_zen = '」』）】＞',
+  leading_zen = '、。：「『（【＜',
 }
 
 M.enabled = true
@@ -11,69 +11,69 @@ M.enabled = true
 M.setup = function(args) M.config = vim.tbl_deep_extend('force', M.config, args or {}) end
 
 M.build_regexp = function()
-	local hOfhz = '[[:alpha:]]'
-	local zOfhz = '[^\\u0-\\u7f' .. M.config.trailing_zen .. ']'
-	local zOfzh = '[^\\u0-\\u7f' .. M.config.leading_zen .. ']'
-	local hOfzh = '[[:alpha:]]'
-	local pattern = '\\v(' .. hOfhz .. '@<=' .. zOfhz .. ')|(' .. zOfzh .. '@<=' .. hOfzh .. ')'
+  local hOfhz = '[[:alpha:]]'
+  local zOfhz = '[^\\u0-\\u7f' .. M.config.trailing_zen .. ']'
+  local zOfzh = '[^\\u0-\\u7f' .. M.config.leading_zen .. ']'
+  local hOfzh = '[[:alpha:]]'
+  local pattern = '\\v(' .. hOfhz .. '@<=' .. zOfhz .. ')|(' .. zOfzh .. '@<=' .. hOfzh .. ')'
 
-	return pattern
+  return pattern
 end
 
 M.search = function() vim.fn.setreg('/', M.build_regexp()) end
 
 M.execute = function()
-	local win = vim.api.nvim_get_current_win()
-	local ok, prevmatchid = pcall(vim.api.nvim_win_get_var, win, 'prevmatchid')
-	if ok and prevmatchid ~= -1 then
-		vim.fn.matchdelete(prevmatchid)
-		vim.api.nvim_win_del_var(win, 'prevmatchid')
-	end
+  local win = vim.api.nvim_get_current_win()
+  local ok, prevmatchid = pcall(vim.api.nvim_win_get_var, win, 'prevmatchid')
+  if ok and prevmatchid ~= -1 then
+    vim.fn.matchdelete(prevmatchid)
+    vim.api.nvim_win_del_var(win, 'prevmatchid')
+  end
 
-	if not M.enabled then return end
+  if not M.enabled then return end
 
-	vim.api.nvim_win_set_var(win, 'prevmatchid', vim.fn.matchadd(M.config.highlight, M.build_regexp(), 0, -1))
+  vim.api.nvim_win_set_var(win, 'prevmatchid', vim.fn.matchadd(M.config.highlight, M.build_regexp(), 0, -1))
 end
 
 M.add_to_qf = function()
-	local pattern = M.build_regexp()
+  local pattern = M.build_regexp()
 
-	local bufnr = vim.api.nvim_get_current_buf()
-	local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+  local bufnr = vim.api.nvim_get_current_buf()
+  local lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
-	local qf_items = {}
-	for lnum, line in ipairs(lines) do
-		local col = 0
-		while true do
-			local res = vim.fn.matchstrpos(line, pattern, col)
-			local start_idx = res[2]
-			local end_idx = res[3]
+  local qf_items = {}
+  for lnum, line in ipairs(lines) do
+    local col = 0
+    while true do
+      local res = vim.fn.matchstrpos(line, pattern, col)
+      local start_idx = res[2]
+      local end_idx = res[3]
 
-			if start_idx == -1 then break end
+      if start_idx == -1 then break end
 
-			table.insert(qf_items, {
-				bufnr = bufnr,
-				lnum = lnum,
-				col = start_idx + 1,
-				text = line,
-			})
+      table.insert(qf_items, {
+        bufnr = bufnr,
+        lnum = lnum,
+        col = start_idx + 1,
+        text = line,
+      })
 
-			if end_idx == col then
-				col = col + 1
-			else
-				col = end_idx
-			end
+      if end_idx == col then
+        col = col + 1
+      else
+        col = end_idx
+      end
 
-			if col >= #line then break end
-		end
-	end
+      if col >= #line then break end
+    end
+  end
 
-	if #qf_items > 0 then
-		vim.fn.setqflist(qf_items, 'r')
-		vim.cmd('copen')
-	else
-		vim.notify('No matches found for hzzh.')
-	end
+  if #qf_items > 0 then
+    vim.fn.setqflist(qf_items, 'r')
+    vim.cmd('copen')
+  else
+    vim.notify('No matches found for hzzh.')
+  end
 end
 
 -- あああaaaいいいiii
@@ -81,3 +81,5 @@ end
 -- 'あああ'
 
 return M
+
+-- vim: set et ft=lua sts=2 sw=2 ts=2 :
--- plugin/hzzh-nvim.lua
+++ plugin/hzzh-nvim.lua
@@ -1,21 +1,21 @@
 local M = require('hzzh-nvim')
 
 vim.api.nvim_create_autocmd('BufEnter', {
-	callback = M.execute,
+  callback = M.execute,
 })
 
 vim.api.nvim_create_user_command('HZZHEnable', function()
-	M.enabled = true
-	M.execute()
+  M.enabled = true
+  M.execute()
 end, {})
 
 vim.api.nvim_create_user_command('HZZHDisable', function()
-	M.enabled = false
-	M.execute()
+  M.enabled = false
+  M.execute()
 end, {})
 
 vim.api.nvim_create_user_command('HZZHSearch', function() M.search() end, {})
 
 vim.api.nvim_create_user_command('HZZHAddQF', function() M.add_to_qf() end, {})
 
--- vim: set et ft=lua sts=4 sw=4 ts=4 :
+-- vim: set et ft=lua sts=2 sw=2 ts=2 :
